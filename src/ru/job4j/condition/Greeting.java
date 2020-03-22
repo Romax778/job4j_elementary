b03f26c33f5093c0352914af5b501d683c02157e
romax <romax777f@gmail.com>
2020-03-22 13:37:39 +0000

    Фиксация изменений на github.[#244180

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,12 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+String idea = " I love Java!" + "But im a newbie ";
+int year = 2020;
+idea = " I love Java!" + "But im a newbie " + year;
+
+System.out.println(idea );
+        }
+    }
+
---
+++ src/ru/job4j/condition/ScArea.java
@@ -0,0 +1,19 @@
+package ru.job4j.condition;
+
+public class ScArea {
+
+
+        public static double square(int p, int k) {
+
+            double s = 0;
+
+            s = Math.pow(p/(2.0*(k+1)),2)*k;
+            return s;
+        }
+
+        public static void main(String[] args) {
+            double result1 = square(6, 2);
+            System.out.println(" s = " + result1);
+        }
+    }
+
