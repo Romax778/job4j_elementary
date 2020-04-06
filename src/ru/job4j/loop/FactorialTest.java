1de5959327b68b8c85af433f504dd0efb3da196d
romax <romax777f@gmail.com>
2020-04-06 21:57:58 +0100

    Фиксация изменений на github.[#244180

---
+++ src/ru/job4j/loop/FactorialTest.java
@@ -0,0 +1,22 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+import ru.job4j.condition.Max;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class FactorialTest {
+    @Test
+    public void whenCalculateFactorialForFiveThenOneHundreedTwenty() {
+        int result = Factorial.calc(5);
+        assertThat(result, is(120));
+    }
+
+
+    @Test
+    public void whenCalculateFactorialForZeroThenOne() {
+        int result = Factorial.calc(0);
+        assertThat(result, is(1));
+    }
+}
