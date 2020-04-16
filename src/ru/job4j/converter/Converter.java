bd796969e581e37efb1823b7cc405982c97e71ac
romax <romax777f@gmail.com>
2020-04-16 13:52:58 +0100

    Фиксация изменений на github.[#244180

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -1,17 +1,37 @@
 package ru.job4j.converter;
 
+/**
+ * Class for calculating  exchange rate
+ * @author fetisov
+ * @since 16.04.2019
+ * @version 1
+ */
 public class Converter {
+    /**
+     *
+     * @param value
+     * @return rubleToEuro plus value
+     */
 
     public static int rubleToEuro(int value) {
             int rsl = value / 70;
             return rsl;
         }
 
+    /**
+     *
+     * @param value
+     * @return rubleToDollar plus value
+     */
         public static int rubleToDollar(int value) {
             int rsl = value / 60;
             return rsl;
         }
 
+    /**
+     * Main
+     * @param args-args
+     */
         public static void main(String[] args) {
             int euro = Converter.rubleToEuro(140);
             System.out.println("140 rubles are " + euro + " euro.");
