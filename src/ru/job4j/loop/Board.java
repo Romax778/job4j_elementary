32e125fe238f595f25b69715eac991b4e82faa35
romax <romax777f@gmail.com>
2020-04-17 15:00:44 +0100

    Фиксация изменений на github.[#244180

--- src/ru/job4j/loop/Board.java
+++ src/ru/job4j/loop/Board.java
@@ -3,7 +3,7 @@ package ru.job4j.loop;
 public class Board {
     public static void paint(int width, int height) {
         for (int i = 0; i < width ; i++ ) {
-            for (int j = 0; j < height; j++ ) {
+            for (int j = 0; j < height ; j++ ) {
                 // условие проверки, что писать пробел или X
                 // Выше в задании мы определили закономерность, когда нужно проставлять X
                 if ((i + j) % 2 == 0 )  {
