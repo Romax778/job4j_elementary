2d0b2cfb3606ee5593fe61d4e00f1908df248c2b
romax <romax777f@gmail.com>
2020-04-20 16:55:06 +0100

    Фиксация изменений на github.[#244180

---
+++ src/ru/job4j/array/AlgoArray.java
@@ -0,0 +1,26 @@
+package ru.job4j.array;
+
+import java.util.Arrays;
+
+public class AlgoArray {
+    public static void main(String[] args) {
+        int[] array = new int[] {5, 3, 2, 1, 4};
+        int temp = array[0]; // переменная для временного хранения значение ячейки с индексом 0.
+        array[0] = array[3]; // записываем в ячейку с индексом 0 значение ячейки с индексом 3.
+        array[3] = temp;
+
+
+        for (int i = 0; i < array.length-1 ; i++) {
+            if (array[i] > array[i + 1]) {
+                temp = array[i];
+                array[i] = array[i + 1];
+                array[i+1]= temp;
+            }
+            System.out.println(array[i]);
+
+            }
+
+        }
+
+    }
+
