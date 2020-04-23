3771101814e027f9e2e2cbc840c73203f2e6c62b
romax <romax777f@gmail.com>
2020-04-23 17:00:09 +0100

    Фиксация изменений на github.[#244180

--- src/ru/job4j/array/FindLoop2.java
+++ src/ru/job4j/array/FindLoop2.java
@@ -1,15 +1,11 @@
 package ru.job4j.array;
 
 public class FindLoop2 {
-    public static int indexOf(int[] data, int el) {
 
-        int rst = -1; // если элемента нет в массиве, то возвращаем -1.
-        return rst;
-    }
 
     public static int indexOf(int[] data, int el, int start, int finish) {
         int rst = -1;
-        for(int i = start; i<finish; i++){
+        for(int i = start; i<=finish-1; i++){
             if(data[i]==el){
                 return i;
             }
