bb7825d78b8a2b77a40284ae245727652504d2f9
romax <romax777f@gmail.com>
2020-04-24 10:56:18 +0100

    Фиксация изменений на github.[#244180

---
+++ src/ru/job4j/array/SortSelected.java
@@ -0,0 +1,18 @@
+
+package ru.job4j.array;
+
+import java.util.Arrays;
+
+public class SortSelected {
+
+    public static int[] sort(int[] data) {
+        for (int i = 0; i < data.length; i ++) {
+            int min = MinDiapason.findMin(data, i, data.length);
+            int index = FindLoop2.indexOf(data, min, i, data.length);
+
+           SwitchArray. swap(data,i,index);
+        }
+        return data;
+    }
+
+}
