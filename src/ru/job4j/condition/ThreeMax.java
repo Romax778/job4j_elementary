1c4590184e2b4fe69540048c6b8c7adea9086822
romax <romax777f@gmail.com>
2020-05-04 05:59:40 +0100

    Фиксация изменений на github.[#244180

--- src/ru/job4j/condition/ThreeMax.java
+++ src/ru/job4j/condition/ThreeMax.java
@@ -2,7 +2,7 @@ package ru.job4j.condition;
 
 public class ThreeMax {
     public static int max(int first, int second, int third) {
-        int result = third;
+        int result = first;
         if (first > second && first > third) {
             result = first;
         }
