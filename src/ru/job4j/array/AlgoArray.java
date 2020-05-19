3f201e087c5b26129455549f69de7761d2755dfb
romax778 <romax777f@gmail.com>
2020-05-19 06:56:21 +0100

    Фиксация изменений на github.[#244180

--- src/ru/job4j/array/AlgoArray.java
+++ src/ru/job4j/array/AlgoArray.java
@@ -10,11 +10,11 @@ public class AlgoArray {
         array[3] = temp;
 
 
-        for (int i = 0; i < array.length-1 ; i++) {
+        for (int i = 0; i < array.length - 1; i++) {
             if (array[i] > array[i + 1]) {
                 temp = array[i];
                 array[i] = array[i + 1];
-                array[i+1]= temp;
+                array[i + 1] = temp;
             }
             System.out.println(array[i]);
 
