7d45f312c51c95012a3ee7f9b5bd62816f5ed906
romax778 <romax777f@gmail.com>
2020-05-21 17:56:06 +0100

    Фиксация изменений на github.[#244180

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -1,16 +1,29 @@
 package ru.job4j.condition;
 
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
+
+
 public class Point {
+    private int x;
+    private int y;
 
-    public static double distance(int x1, int y1, int x2, int y2) {
-
-            return Math.pow(Math.pow(x2-x1,2)  +  Math.pow(y2-y1, 2) ,0.5);
-
-        }
-
-        public static void main(String[] args) {
-            double result = distance(1, 3, 7, 8);
-            System.out.println("result (0, 0) to (2, 0) " + result);
-        }
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
     }
+
+    public  double distance(Point that) {
+
+        return (pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2));
+
+    }
+
+    public static void main(String[] args) {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double dist = a.distance(b);
+        System.out.println(dist);
+    }
+}
 
