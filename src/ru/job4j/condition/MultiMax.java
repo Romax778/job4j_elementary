29146ffd812291e00b214b59f350f06c5a1fba3c
romax778 <romax777f@gmail.com>
2020-05-23 06:47:50 +0100

    Фиксация изменений на github.[#244180

--- src/ru/job4j/condition/MultiMax.java
+++ src/ru/job4j/condition/MultiMax.java
@@ -3,7 +3,14 @@ package ru.job4j.condition;
 public class MultiMax {
     public int max(int first, int second, int third) {
         int result = first > second ? first : second;
-        int result2 = result > third ? result : third ;
+        int result2 = result > third ? result : third;
         return result2;
+    }
+
+    public int max(int first, int second, int third, int fourth){
+        int res = first > second ? first : second;
+        int res2 = res > third ? res : third;
+        int res3 = res2 > fourth ? res2 : fourth;
+        return res3;
     }
 }
